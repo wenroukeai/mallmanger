40c8aadcae92f939557a60700e30c94ede0d68ef
wenroukeai <1257787262@qq.com>
2020-10-12 21:42:38 +0800

    侧边栏调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,33 +1,135 @@
 <template>
   <el-container class="container">
+    <!-- 头部 -->
     <el-header class="header">
       <el-row>
         <el-col :span="4" class="left">
-            <div class="grid-content bg-purple">
-                <img width=200px height=60px  src="../../assets/img/icon.png" alt="无法显示图片" srcset="">
-            </div>
+          <div class="grid-content bg-purple">
+            <img
+              width="200px"
+              height="60px"
+              src="../../assets/img/icon.png"
+              alt="无法显示图片"
+              srcset=""
+            />
+          </div>
         </el-col>
         <el-col :span="18" class="middle">
-            <div class="grid-content bg-purple-light">
-                <h3>电商后台管理系统</h3>
-            </div>
+          <div class="grid-content bg-purple-light">
+            <h3>电商后台管理系统</h3>
+          </div>
         </el-col>
         <el-col :span="2" class="right">
-            <div class="grid-content bg-purple">
-                <a class="loginout" href="#">退出</a>
-            </div>
+          <div class="grid-content bg-purple">
+            <a class="loginout" href="#">退出</a>
+          </div>
         </el-col>
       </el-row>
     </el-header>
     <el-container class="">
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <!-- 侧边栏 -->
+
+      <el-aside class="aside" width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened=true
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-location"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index="2-1">
+              <i class="el-icon-location"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-location"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index="3-1">
+              <i class="el-icon-location"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-location"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-location"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index="4-1">
+              <i class="el-icon-location"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="5-1">
+              <i class="el-icon-location"></i>
+              <span>可视化分析</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主要内容 -->
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style scope>
@@ -45,11 +147,11 @@ export default {};
 }
 
 /* 头部样式 */
-    .left{
-        text-align: left;
-    }
-    .loginout{
-        line-height: 60px;
-        text-decoration: none;
-    }
+.left {
+  text-align: left;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
